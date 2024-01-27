3266af4d4c29612938c1b494d128096348c88fdb
leo <113992401+defineprogramming@users.noreply.github.com>
2024-01-27 10:53:16 +0000

    Fix FA 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function MainComponent() {
   const features = [
@@ -133,13 +134,14 @@ function MainComponent() {
 
   return (
     <>
-      <Head>
-        <title>Verbatim Social</title>
+      <Helmet>
         <link
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
           rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          integrity="sha384-k6RqeWec7J7hQHfk5Y6A7joxTCcQbSTH6lR3T9tM6i5MvRbRB9u8JLjVDN2fW3Jv"
+          crossOrigin="anonymous"
         />
-      </Head>
+      </Helmet>
       <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#030303] to-[#121212] text-white font-roboto">
       <header className="py-8 px-6 md:px-12 border-b border-gray-700 relative">
         <div className="container mx-auto flex items-center justify-between">
