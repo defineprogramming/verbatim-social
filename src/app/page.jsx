bac9a750d9a8067d5a5e1ffd3b34ab00db2ff2aa
leo <113992401+defineprogramming@users.noreply.github.com>
2024-01-27 10:57:06 +0000

    Remove integrity

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -138,7 +138,6 @@ function MainComponent() {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          integrity="sha384-k6RqeWec7J7hQHfk5Y6A7joxTCcQbSTH6lR3T9tM6i5MvRbRB9u8JLjVDN2fW3Jv"
           crossOrigin="anonymous"
         />
       </Helmet>
